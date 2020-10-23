7bcccd9077f4cb4b3cac08d28826a6a400e69592
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-10-23 15:40:15 +0530

    for indexing

--- for.go
+++ for.go
@@ -3,12 +3,18 @@ package main
 import "fmt"
 
 func main() {
-    start := 1
-    things := []string{"matter", "bolso", "boleto", "vaso", "casa"}
+	start := 1
+	things := []string{"matter", "bolso", "boleto", "vaso", "casa"}
 
-    fmt.Println(things)
+	fmt.Println(things)
 
-    for i:= 0; i<10; i++{
-        fmt.Println(i+start)
-    }
+	for i := 0; i < 10; i++ {
+		fmt.Println(i + start)
+	}
+
+	for i := 0; i < len(things); i++ {
+		fmt.Println(things[i])
+
+	}
+
 }
