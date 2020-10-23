b60b674b89d6f31cfe3032656376a06f54eb1a05
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-10-23 12:38:19 +0530

    map

---
+++ map.go
@@ -0,0 +1,17 @@
+package main 
+ 
+ import "fmt"
+
+ func main() {
+     score := make(map[string]int)
+     score["Akash"] = 89
+     fmt.Println(score)
+
+     score["john"] = 34
+     score["tom"] = 23
+     score["leo"] = 56
+     score["jordan"] = 78
+     fmt.Println(score)
+
+
+ }
